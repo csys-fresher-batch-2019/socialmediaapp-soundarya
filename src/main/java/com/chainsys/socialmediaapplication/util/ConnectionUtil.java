cf9ddb7f18a1b4a64aae53ccd454926850281616
Soundarya P <soundmithu@gmail.com>
2020-02-17 09:46:11 +0530

    Updated changes

--- src/main/java/com/chainsys/socialmediaapplication/util/ConnectionUtil.java
+++ src/main/java/com/chainsys/socialmediaapplication/util/ConnectionUtil.java
@@ -3,6 +3,9 @@ package com.chainsys.socialmediaapplication.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
 import com.chainsys.socialmediaapplication.daoimpl.Logger;
 
 public class ConnectionUtil {
@@ -13,6 +16,12 @@ public class ConnectionUtil {
 		Logger logger=new Logger();
 		logger.debug("connected");
 		return con;
+	}
+	public static Jdbi getJdbi() throws Exception {
+		Connection connection = ConnectionUtil.conMethod();
+		Jdbi jdbi = Jdbi.create(connection);
+		jdbi.installPlugin(new SqlObjectPlugin());
+		return jdbi;
 	}
 
 }
